462efd1a2ee217b8a70d1769f2fae9265f54fc4f
Daniel Bälz <me@dbaelz.de>
2017-07-04 01:15:11 +0200

    Add pyyaml and use lower case lib names

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-            "Click"
+            "click",
+            "pyyaml"
     ],
     entry_points={
          'console_scripts': ['adbons=src.adbons:cli']
