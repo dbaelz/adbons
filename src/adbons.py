1575e209d8302105523c2b57c23228fa63bd13a2
Daniel Bälz <me@dbaelz.de>
2017-07-05 21:15:05 +0200

    Move set-app and set-device into config command

--- src/adbons.py
+++ src/adbons.py
@@ -6,27 +6,31 @@ from .config import (SECTION_APP, SECTION_DEVICE, KEY_DEFAULT)
 
 
 @click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx,):
+    """A wrapper for the adb tool. It's just adb on steroids."""
+    ctx.invoked_subcommand
+
+
+@cli.command()
 @click.option("-a", "--set-app", type=click.STRING,
               help="Set a default app id.")
 @click.option("-d", "--set-device", type=click.STRING,
               help="Set a default device id.")
 @click.pass_context
-def cli(ctx, set_app, set_device):
-    """A wrapper for the adb tool. It's just adb on steroids."""
-    if ctx.invoked_subcommand is None:
-        if set_app:
-            write_value(SECTION_APP, KEY_DEFAULT, set_app)
-        if set_device:
-            # TODO: Check if the selected device is attached
-            # TODO: Only add it in this case or show prompt
-            write_value(SECTION_DEVICE, KEY_DEFAULT, set_device)
-    else:
-        ctx.invoked_subcommand
+def config(ctx, set_app, set_device):
+    """Configure adbons"""
+    if set_app:
+        write_value(SECTION_APP, KEY_DEFAULT, set_app)
+    if set_device:
+        # TODO: Check if the selected device is attached
+        # TODO: Only add it in this case or show prompt
+        write_value(SECTION_DEVICE, KEY_DEFAULT, set_device)
 
 
 @cli.command("devices")
 def list():
-    """List all attached devices"""
+    """List all attached devices."""
     list_devices()
 
 
