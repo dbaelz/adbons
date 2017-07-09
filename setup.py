61ffffefb2000dae4f4dbb3f76e0c7b750f7d462
Daniel Bälz <me@dbaelz.de>
2017-07-09 12:40:47 +0200

    Expand setup files for release of version 0.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     author_email="me@dbaelz.de",
     description="""A wrapper for the Android adb tool.
         It's just adb on steroids""",
+    keywords="android debug bridge adb",
     license="BSD",
+    url="https://github.com/dbaelz/adbons",
+    python_requires='>=3.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -18,7 +21,12 @@ setup(
          'console_scripts': ['adbons=src.adbons:cli']
     },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers"
     ],
 )
