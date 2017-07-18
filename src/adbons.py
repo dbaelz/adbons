ade2a5d2e0cc2988bc0a480d1f274c91f3b16458
Daniel Bälz <me@dbaelz.de>
2017-07-19 00:53:31 +0200

    Add screen capture into output file as png

--- src/adbons.py
+++ src/adbons.py
@@ -2,7 +2,8 @@ import click
 
 from .commands.config_commands import config
 from .commands.adb_commands import (adb_command, list_devices, kill, kill_all,
-                                    clear, input_text, input_keyevent)
+                                    clear, input_text, input_keyevent,
+                                    screencap)
 
 
 @click.group(invoke_without_command=True)
@@ -24,3 +25,4 @@ cli.add_command(kill_all)
 cli.add_command(clear)
 cli.add_command(input_text)
 cli.add_command(input_keyevent)
+cli.add_command(screencap)
